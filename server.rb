8be9d492f997b340647d42da40e58da19dbe8b7a
Maya Driver <mayadrivermd@gmail.com>
2014-12-15 18:43:11 +0000

    sinatra stuff

--- server.rb
+++ server.rb
@@ -5,7 +5,13 @@ require './lib/basket'
 class Supermarket < Sinatra::Base
 
   get '/' do
+    @basket = Basket.new
     erb :index
+  end
+
+  post '/cart' do
+    @basket = Basket.new
+    erb :cart
   end
 
 end
